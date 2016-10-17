a9510ba5714622606132fce0fbba23ea11de5204
Denis <denis_davidyuk@hotmail.com>
2016-10-17 18:37:39 +1000

    Speedup by sending requests in single session
    According: http://docs.python-requests.org/en/master/user/advanced/

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from parsers import groups_parser, student_list_parser, student_parser
 from models import Student, Group, Discipline, Task, Mark
 
 baseUrl = 'http://imcs.dvfu.ru/works/'
+session = requests.Session()
 
-groups = list(map(lambda x: Group.create_or_get(**x)[0], groups_parser(requests.get(baseUrl + 'students').content)))
+groups = list(map(lambda x: Group.create_or_get(**x)[0], groups_parser(session.get(baseUrl + 'students').content)))
 print('Found {} groups'.format(len(groups)))
 
 for group in groups:
     data = {'group': group.id, 'student_name': '', 'do_select': 'Выбрать'}
     students = list(map(lambda x: Student.create_or_get(**x)[0].groups.add(group),
-                        student_list_parser(requests.post(baseUrl + 'students', data=data).content)))
+                        student_list_parser(session.post(baseUrl + 'students', data=data).content)))
     sys.stdout.write('\rFound {} students in {}'.format(len(students), group.name))
 
 students = Student.select()
@@ -20,7 +21,7 @@ sys.stdout.write('\rFound {} students\n'.format(len(students)))
 
 count = 0
 for student in students:
-    for disciplineDict in student_parser(requests.get(baseUrl + 'marks_student?id={}'.format(student.id)).content):
+    for disciplineDict in student_parser(session.get(baseUrl + 'marks_student?id={}'.format(student.id)).content):
         tasks = disciplineDict.pop('tasks')
         (discipline, _) = Discipline.create_or_get(**disciplineDict)
         for taskDict in tasks:
